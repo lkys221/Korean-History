96c7501d4b8cc6ba84dd948bde9ce5c0a84e3cd3
Leo .Kim <lkys221@snu.ac.kr>
2018-05-29 20:59:32 +0900

    init

---
+++ app/src/main/java/org/androidtown/myapplication/MenuActivity.java
@@ -0,0 +1,22 @@
+package org.androidtown.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void onBtn1Clicked(View v){
+        Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
+        startActivity(intent);
+    }
+
+
+}

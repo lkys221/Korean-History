6e6a5ee4b16cb64f4b4a441741ea53d577bcb67d
lkys221 <lkys221@snu.ac.kr>
2018-06-11 22:35:34 +0900

    exam list

---
+++ app/src/main/java/org/androidtown/myapplication/HomeFragment.java
@@ -0,0 +1,24 @@
+package org.androidtown.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+/**
+ * Provides UI for the view with List.
+ */
+public class HomeFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.homepage, null);
+    }
+
+
+}
--- app/src/main/java/org/androidtown/myapplication/MenuActivity.java
+++ app/src/main/java/org/androidtown/myapplication/MenuActivity.java
@@ -1,37 +1,70 @@
 package org.androidtown.myapplication;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-import org.androidtown.myapplication.QuizActivity;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuActivity extends AppCompatActivity {
-    SharedPreferences pref;
-    SharedPreferences.Editor editor;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        // Adding Toolbar to Main screen
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Setting ViewPager for each Tabs
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        // Set Tabs inside Toolbar
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
     }
 
-    public void onBtn1Clicked(View v){
-        Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
-        startActivity(intent);
+    // Add Fragments to Tabs
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), "Home");
+        adapter.addFragment(new ExamFragment(), "Exam");
+        adapter.addFragment(new MypageFragment(), "My Page");
+        viewPager.setAdapter(adapter);
     }
 
-    public void onDataBtnClicked(View v){
-        pref = getSharedPreferences("Choices", Context.MODE_PRIVATE);
-        editor = pref.edit();
-        editor.clear();
-        editor.commit();
-        Toast.makeText(getApplicationContext(), "Deleted", Toast.LENGTH_SHORT).show();
-    }
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
 
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+
+    }
 }

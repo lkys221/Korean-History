53316414b476f8d6712e04ecd1c2f9fb512611d4
lkys221 <lkys221@snu.ac.kr>
2018-06-22 21:38:59 +0900

    result, compare pref

--- app/src/main/java/org/androidtown/myapplication/QuizActivity.java
+++ app/src/main/java/org/androidtown/myapplication/QuizActivity.java
@@ -22,8 +22,8 @@ public class QuizActivity extends AppCompatActivity {
     ViewGroup layout_choice1, layout_choice2, layout_choice3, layout_choice4, layout_choice5;
     ScrollView scrollView;
 
-    SharedPreferences choicePref, resultPref;
-    SharedPreferences.Editor choiceEditor, resultEditor;
+    SharedPreferences choicePref, resultPref, comparePref;
+    SharedPreferences.Editor choiceEditor, resultEditor, compareEditor;
 
 
     String[] questionArr = {"1.교사의 질문에대한 답변으로 가장 적절한 것은?", "2. (가), (나) 나라에 대한 설명으로 옳은 것은?", "3. 다음 법을 시행하였던 나라에 대한 설명으로 옳은 것은?", "4. 밑줄 그은 ‘대책’으로 옳은 것은?", "5.다음 사건이 일어난 이후의 시실로 옳은 것은?", "6.(개, (나) 사이의 시기에 있었던 사실로 옳은 것은? ", "7.다음 자료에 나타난 시기의사실로 옳은 것은?", "8.(가) 왕에 대한 설명으로 옳은 것은?", "9. (가) 국가의 경제 싱황에 대한 설명으로 옳은 것은?",
@@ -34,11 +34,11 @@ public class QuizActivity extends AppCompatActivity {
             {"신지,읍차 등의 지배자가 있었다.", "골품제라는 신분 제도를 마련하였다.", "제가 회의에서 국가 중대사를 결정하였다.", "왕 아래상, 대부‘ 장군 등의 관직을 두었다.", "여러 가(加)들이 별도로 사출도를 주관하였다."},
             {"진대법을 실시하여 빈민을 구제하였다.", "상평창을 설치하여 물기를 조절하였다.", "구황촬요를 간행하여 기근에 대비하였다.", "구제도감을 설립하여 백성을 구호하였다.", "혜민국을 마련하여 병자에게 익뇨을 지급하였다."},
             {"고구려가 옥저를 복속시켰다.", "백제가 고구려의 평양성을 공격하였다.", "가야 연맹이 대가야를 중심으로 재편되었다.", "신라 지배자의 칭호가 차치-웅으로 바뀌었다.", "고구려가 대빙-군을 축출하고 영토를 확장하였다."},
-            {"무령왕이 22담로에 왕족을 파견히-였다." ,"침류왕이 동진으로부터 불교를 수용하였다.", "의자왕이 신리를 공격하여 대야성을 함락하였다.", "고이왕이 좌평과 관등제의 기본 골격을 마련하였다.", "성왕이고구려를 공격하여한강 유역을 수복하였다."},
+            {"무령왕이 22담로에 왕족을 파견히-였다.", "침류왕이 동진으로부터 불교를 수용하였다.", "의자왕이 신리를 공격하여 대야성을 함락하였다.", "고이왕이 좌평과 관등제의 기본 골격을 마련하였다.", "성왕이고구려를 공격하여한강 유역을 수복하였다."},
             {"복신과 도침 등이주류성에서 군사를 일으켰다.", "묘청 등이중심이 되어 서경 천도를 주장하였다.", "신리-군이 당의 군대에 맞서 매소성에서 승리하였다.", "지방에서 호족들이반독립적인 세력으로 성장하였다.", "요세가 법화 신앙에중점을 둔 백련 결사를 주도하였다."},
             {"화랑도를 국가 조직으로 개편하였다.", "이사부를 보내 우산국을 복속시켰다.", "건원이라는 독자적인 연호를 시용하였다.", "관리에게 관료전을 지급하고 녹읍을 폐지하였다.", "호국의 염원을 담아 황룡사 구층 목탑을 건립하였다."},
             {"울산항이국제 무역헝-으로 번성하였다.", "특산품으로 솔빈부의 말이유명하였다.", "청해진을 설치하여해상 무역을 전개하였다.", "건원중보를 발행하여 화폐 유통을 추진하였다.", "시장을 관리하는 관청인 동시전을 설치하였다."},
-            {"(가) -흑창을 설치하여 민생을 안정시켰다.", "(가) -광덕, 준풍 등의 독자적인 연호를 시용하였다.", "(나) - 12목을 설치하고 지방관을 파견하였다." ,"(나) -상수리 제도를 실시하여 지방 세력을통제하였다.", "(가), (나) -현직 관리에게 전지와 시지를 지급하였다."},
+            {"(가) -흑창을 설치하여 민생을 안정시켰다.", "(가) -광덕, 준풍 등의 독자적인 연호를 시용하였다.", "(나) - 12목을 설치하고 지방관을 파견하였다.", "(나) -상수리 제도를 실시하여 지방 세력을통제하였다.", "(가), (나) -현직 관리에게 전지와 시지를 지급하였다."},
             {"go_39_11_01", "go_39_11_02", "go_39_11_03", "go_39_11_04", "go_39_11_05"},
             {"권수정혜결사문을 작성하여 정혜쌍수를 강조하였다.", "해동고승전을 집필하여승려들의 전기를 기록하였다", "보현십원가를 지어 불교 교리를 대중에게 전피-하였다.", "교관겸수를 내세워 이론 연마와 실천을 함께중시하였다.", "삼국유사를 저술하여 불교 중심의 민간 설회를 정리하였다."},
             {"광군을 창설하여외침에대비하였다.", "거란의 침략을 피해 왕이나주로 피난하였다.", "서희가 외교 담판을 벌여강동 6주를 획득하였다.", "만부교 사건이 일어나 거란과의관계가 악화되었다.", "후주와 사신을 교환히·여 대외 관계의 안정을 꾀하였다."},
@@ -53,9 +53,6 @@ public class QuizActivity extends AppCompatActivity {
     int score = 0;
 
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,13 +64,16 @@ public class QuizActivity extends AppCompatActivity {
         resultPref = getSharedPreferences("Results", Context.MODE_PRIVATE);
         resultEditor = resultPref.edit();
 
+        comparePref = getSharedPreferences("Compares", Context.MODE_PRIVATE);
+        compareEditor = comparePref.edit();
+
 
         //Bottom Navigation Bar
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.action_previous:
                         onPreviousBtnClicked();
                         break;
@@ -118,7 +118,7 @@ public class QuizActivity extends AppCompatActivity {
         layout_choice4 = (ViewGroup) findViewById(R.id.layout_choice4);
         layout_choice5 = (ViewGroup) findViewById(R.id.layout_choice5);
 
-        if(loadChoice(0) != 0)
+        if (loadChoice(0) != 0)
             makeChoiceSelected(loadChoice(0));
 
         layout_choice1.setOnClickListener(new View.OnClickListener() {
@@ -159,9 +159,9 @@ public class QuizActivity extends AppCompatActivity {
 
     }
 
-    public boolean onNextBtnClicked(){
+    public boolean onNextBtnClicked() {
         //save current question's choice
-        if(choice != 0)
+        if (choice != 0)
             saveChoices(index, choice);
 
         //to next question
@@ -169,14 +169,13 @@ public class QuizActivity extends AppCompatActivity {
         choice = 0;
 
         //after last question
-        if(index > questionArr.length - 1){
+        if (index > questionArr.length - 1) {
             checkAnswer();
             Toast.makeText(getApplicationContext(), "Your score is " + score + "/" + questionArr.length, Toast.LENGTH_LONG).show();
             gotoResultPage();
             finish();
             return false;
-        }
-        else {
+        } else {
             //scroll up
             scrollView.fullScroll(ScrollView.FOCUS_UP);
             //next question
@@ -192,18 +191,18 @@ public class QuizActivity extends AppCompatActivity {
             makeChoiceSelected(finalChoice);
         }
 
-                return true;
+        return true;
     }
 
-    public boolean onPreviousBtnClicked(){
+    public boolean onPreviousBtnClicked() {
         //save current question's choice
-        if(choice != 0)
+        if (choice != 0)
             saveChoices(index, choice);
 
         index--;
         choice = 0;
 
-        if(index < 0){
+        if (index < 0) {
             Toast.makeText(getApplicationContext(), "This is the first question", Toast.LENGTH_LONG).show();
             index++;
             return false;
@@ -228,7 +227,7 @@ public class QuizActivity extends AppCompatActivity {
 
     //choices 관련 method
     private boolean setChoicesText(int index) {
-        if(choiceArr[index][0].startsWith("go")){
+        if (choiceArr[index][0].startsWith("go")) {
             clearTextChoices();
 
             String imageName = choiceArr[index][0];
@@ -252,8 +251,7 @@ public class QuizActivity extends AppCompatActivity {
             choice5.setCompoundDrawablesWithIntrinsicBounds(resID, 0, 0, 0);
 
             return true;
-        }
-        else {
+        } else {
             clearImageChoices();
 
             choice1.setText(choiceArr[index][0]);
@@ -267,29 +265,35 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void makeChoiceSelected(int finalChoice) {
-        switch (finalChoice){
-            case 0: emptyChoices();
+        switch (finalChoice) {
+            case 0:
+                emptyChoices();
                 choice = 0;
                 break;
-            case 1: clickChoice(layout_choice1);
+            case 1:
+                clickChoice(layout_choice1);
                 break;
-            case 2: clickChoice(layout_choice2);
+            case 2:
+                clickChoice(layout_choice2);
                 break;
-            case 3: clickChoice(layout_choice3);
+            case 3:
+                clickChoice(layout_choice3);
                 break;
-            case 4: clickChoice(layout_choice4);
+            case 4:
+                clickChoice(layout_choice4);
                 break;
-            case 5: clickChoice(layout_choice5);
+            case 5:
+                clickChoice(layout_choice5);
                 break;
         }
     }
 
-    public void clickChoice(ViewGroup v){
-            emptyChoices();
-            v.setBackgroundResource(R.drawable.layout_clicked);
+    public void clickChoice(ViewGroup v) {
+        emptyChoices();
+        v.setBackgroundResource(R.drawable.layout_clicked);
     }
 
-    public void emptyChoices(){
+    public void emptyChoices() {
         layout_choice1.setBackgroundResource(R.drawable.layout_rc_primary);
         layout_choice2.setBackgroundResource(R.drawable.layout_rc_primary);
         layout_choice3.setBackgroundResource(R.drawable.layout_rc_primary);
@@ -297,33 +301,46 @@ public class QuizActivity extends AppCompatActivity {
         layout_choice5.setBackgroundResource(R.drawable.layout_rc_primary);
     }
 
-    private void saveChoices(int index, int choiceNum){
-        choiceEditor.putInt(""+index, choiceNum);
+    private void saveChoices(int index, int choiceNum) {
+        choiceEditor.putInt("" + index, choiceNum);
         choiceEditor.commit();
     }
 
-    private int loadChoice(int index){
-        int finalChoice = choicePref.getInt(""+index, 0);
+    private int loadChoice(int index) {
+        int finalChoice = choicePref.getInt("" + index, 0);
         return finalChoice;
     }
 
-    private void deleteData(){
+    private void gotoResultPage() {
+        resultEditor.putInt("go_39", score);
+        resultEditor.commit();
+
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void deleteData() {
         choiceEditor.clear();
     }
 
-    private void checkAnswer(){
+    private void checkAnswer() {
         int userAns;
 
         for (int i = 0; i < questionArr.length; i++) {
             userAns = loadChoice(i);
-            if(userAns == answerArr[i]) {
+            if (userAns == answerArr[i]) {
                 score++;
+                compareEditor.putInt("" + i, 1);
+            } else {
+                compareEditor.putInt("" + i, 0);
             }
         }
+        compareEditor.commit();
 
     }
 
-    private void clearImageChoices(){
+    private void clearImageChoices() {
         choice1.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
         choice2.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
         choice3.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
@@ -331,7 +348,7 @@ public class QuizActivity extends AppCompatActivity {
         choice5.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
     }
 
-    private void clearTextChoices(){
+    private void clearTextChoices() {
         choice1.setText("");
         choice2.setText("");
         choice3.setText("");
@@ -339,9 +356,6 @@ public class QuizActivity extends AppCompatActivity {
         choice5.setText("");
     }
 
-    private void gotoResultPage(){
-        Intent intent = new Intent(this, ResultActivity.class);
-        startActivity(intent);
-    }
-
 }
+
+

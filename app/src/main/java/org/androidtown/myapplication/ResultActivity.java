c9d811913d3135df2f811ea593f7886a0002f614
lkys221 <lkys221@snu.ac.kr>
2018-06-22 22:17:26 +0900

    score result

--- app/src/main/java/org/androidtown/myapplication/ResultActivity.java
+++ app/src/main/java/org/androidtown/myapplication/ResultActivity.java
@@ -1,15 +1,24 @@
 package org.androidtown.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import io.netopen.hotbitmapgg.library.view.RingProgressBar;
 
 public class ResultActivity extends AppCompatActivity {
     RingProgressBar ringProgressBar;
-    int score = 10;
+    int score;
+    TextView textView;
+
+    SharedPreferences choicePref, resultPref, comparePref;
+    SharedPreferences.Editor choiceEditor, resultEditor, compareEditor;
+
+
 
     Handler myHandler = new Handler(){
         @Override
@@ -23,6 +32,20 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        choicePref = getSharedPreferences("Choices", Context.MODE_PRIVATE);
+        choiceEditor = choicePref.edit();
+
+        resultPref = getSharedPreferences("Results", Context.MODE_PRIVATE);
+        resultEditor = resultPref.edit();
+
+        comparePref = getSharedPreferences("Compares", Context.MODE_PRIVATE);
+        compareEditor = comparePref.edit();
+
+        score = resultPref.getInt("go_39", 0);
+        textView = (TextView) findViewById(R.id.textview1);
+
+
 
         ringProgressBar = (RingProgressBar) findViewById(R.id.progressbar);
         ringProgressBar.setOnProgressListener(new RingProgressBar.OnProgressListener() {
@@ -43,6 +66,8 @@ public class ResultActivity extends AppCompatActivity {
                 }
             }
         }).start();
+
+        textView.setText(score + " / 15");
 
 
     }

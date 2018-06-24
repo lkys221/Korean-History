ddba28c7bff64a0c3425a70ea5760b8a7102dc85
lkys221 <lkys221@snu.ac.kr>
2018-06-24 22:03:07 +0900

    array adapter

--- app/src/main/java/org/androidtown/myapplication/ResultActivity.java
+++ app/src/main/java/org/androidtown/myapplication/ResultActivity.java
@@ -1,16 +1,21 @@
 package org.androidtown.myapplication;
 
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import io.netopen.hotbitmapgg.library.view.RingProgressBar;
 
-public class ResultActivity extends AppCompatActivity {
+public class ResultActivity extends ListActivity {
     RingProgressBar ringProgressBar;
     int score;
     TextView textView;
@@ -18,7 +23,9 @@ public class ResultActivity extends AppCompatActivity {
     SharedPreferences choicePref, resultPref, comparePref;
     SharedPreferences.Editor choiceEditor, resultEditor, compareEditor;
 
-
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    final int NumOfQuestion = 15;
 
     Handler myHandler = new Handler(){
         @Override
@@ -35,18 +42,19 @@ public class ResultActivity extends AppCompatActivity {
 
         choicePref = getSharedPreferences("Choices", Context.MODE_PRIVATE);
         choiceEditor = choicePref.edit();
-
         resultPref = getSharedPreferences("Results", Context.MODE_PRIVATE);
         resultEditor = resultPref.edit();
-
         comparePref = getSharedPreferences("Compares", Context.MODE_PRIVATE);
         compareEditor = comparePref.edit();
 
         score = resultPref.getInt("go_39", 0);
         textView = (TextView) findViewById(R.id.textview1);
 
+        adapter = new ArrayAdapter<String>(this, R.layout.question_on_result, listItems);
+        setListAdapter(adapter);
 
 
+        //ring progress bar
         ringProgressBar = (RingProgressBar) findViewById(R.id.progressbar);
         ringProgressBar.setOnProgressListener(new RingProgressBar.OnProgressListener() {
             @Override
@@ -69,6 +77,14 @@ public class ResultActivity extends AppCompatActivity {
 
         textView.setText(score + " / 15");
 
-
     }
+
+    public void addItems(View v) {
+        for (int i = 0; i < NumOfQuestion; i++) {
+            listItems.add("Question " + (i+1));
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+
 }
